cce6c555f2046d79632c3d7a0b934b8fada6d459
KE Jiang <yuhioh217@gmail.com>
2020-01-02 18:39:34 +0800

    Add GetPreTenAverageAmount funtion

--- packets/strategy/model2.go
+++ packets/strategy/model2.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/go-gota/gota/dataframe"
@@ -39,8 +40,8 @@ func (sb *SBratio) AddBuy(b int) {
 	sb.Buy = sb.Buy + b
 }
 
-func (sb *SBratio) UpdateTotal(t int) {
-	sb.Total = t
+func (sb *SBratio) AddTotal(t int) {
+	sb.Total = sb.Total + t
 }
 
 func (sb *SBratio) GetSratio() float64 {
@@ -72,36 +73,76 @@ func checkError(err error) {
 
 func Model2Calculate() {
 	sbRatio := GetSBratioInstance()
-	timeArr = df.Col("Time").String()
-	//da := df.Col("Deal").Float()    // deal array
+	//timeArr := df.Col("Time").String()
+	da := df.Col("Deal").Float()    // deal array
 	ta, err := df.Col("Type").Int() // type array
 	checkError(err)
 	oca, err := df.Col("OrderCount").Int() // order count array
 	checkError(err)
-	tca, err := df.Col("TotalCount").Int() // total count array
-	checkError(err)
+	//tca, err := df.Col("TotalCount").Int() // total count array
+	//checkError(err)
 
 	switch ta[len(ta)-1] { // use the latest packets type
 	case 1: // sell deal
 		sbRatio.AddSell(oca[len(oca)-1])
-		sbRatio.UpdateTotal(tca[len(tca)-1])
+		sbRatio.AddTotal(oca[len(oca)-1])
 		break
 	case 2: // buy deal
 		sbRatio.AddBuy(oca[len(oca)-1])
-		sbRatio.UpdateTotal(tca[len(tca)-1])
+		sbRatio.AddTotal(oca[len(oca)-1])
 		break
 	default:
 		break
 	}
+	fmt.Println(sbRatio)
+	if bratio, sratio := sbRatio.GetBratio(), sbRatio.GetSratio(); bratio != 0 && sratio != 0 {
+		if len(oca) > 5 { // do nothing before 9:00:00 - 9:00:30 > about 5 packets data
+			//if (sratio / bratio) < 0.67 { // 40%/60% -> go to sell
+			lastAmount := int(da[len(da)-1] * 1000 * float64(oca[len(oca)-1]))
+			fmt.Println("lastAmount : ", lastAmount)
+			// if current trading amount is over pre-ten trading average amount
+			if lastAmount >= GetPreTenAverageAmount(da, oca)*2 {
+				fmt.Println("Strong buy")
+			}
+			//}
+		}
+	}
+}
+
+func GetContinuousTenSlope() float64 {
+	return 0.0
+}
+
+func GetPreTenAverageAmount(dealArr []float64, ocaArr []int) int {
+	fmt.Print("Get the pre-ten deals average amount : ")
+	sum := 0.0
+	for i := len(dealArr) - 2; i > len(dealArr)-10 && i >= 0; i-- {
+		// fmt.Printf("%.2f * %d \n", dealArr[i], ocaArr[i])
+		sum += dealArr[i] * 1000 * float64(ocaArr[i])
+	}
+	fmt.Println(int(sum / float64(len(dealArr))))
+	return int(sum / float64(len(dealArr)))
 }
 
 func main() {
-	rows := []Info{
+
+	rows := []Info{}
+
+	test := []Info{
 		{"13:22:24", 36.75, 1, 2, 2},
 		{"13:22:29", 36.75, 1, 5, 7},
-		{"13:22:34", 36.80, 1, 9, 16},
-		{"13:22:39", 36.75, 2, 18, 34},
+		{"13:22:34", 36.80, 2, 9, 16},
+		{"13:22:39", 36.75, 1, 18, 34},
+		{"13:22:39", 36.80, 2, 2, 34},
+		{"13:22:39", 36.75, 1, 11, 34},
+		{"13:22:39", 36.80, 2, 40, 34},
+		{"13:22:39", 36.80, 2, 70, 34},
+		{"13:22:39", 36.85, 2, 2, 34},
+		{"13:22:39", 36.80, 1, 12, 34},
 	}
 
-	NewModel2Dataframe(rows)
+	for _, t := range test {
+		rows = append(rows, t)
+		NewModel2Dataframe(rows)
+	}
 }
